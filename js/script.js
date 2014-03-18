52e08b28da544ceb257a8316e83fd7387b15852d
Rashaad Ramdeen <rashaad.ramdeen@gmail.com>
2014-03-18 03:57:13 -0400

    fixed a lot of bugs with the homepage banner

--- js/script.js
+++ js/script.js
@@ -5,9 +5,32 @@ $(function(){
 	//smooth scrolling
 	$("html").niceScroll();
 
+	function valign(el, min_height) {
+		if(min_height) {
+			$(el).css('margin-top', (min_height/2) - ($(el).height()/2) + 35 + 'px');
+		}else{
+			$(el).css('margin-top', ($(window).height()/2) - ($(el).height()/2) + 27 + 'px');
+		}
+		
+	}
+
+	
+	$( window ).resize(function() {
+		$("#fader").css('height', $(window).height());
+		//center banner text
+		$("#fader ul#slides li").each(function(index, el){
+			valign($(this));
+		});
+	});
+
+	//center banner text
+	$("#fader ul#slides li").each(function(index, el){
+		valign($(this));
+	});
+
 });
 
 window.onload = function(){
-	//var fader = new Fader('#slides li', '#background-images li', 5000);
+	//var fader = new Fader('#slides li', '#background-images li', 10000);
 	//fader.start();
 };
