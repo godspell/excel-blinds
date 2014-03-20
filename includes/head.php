d90602875fd4604d7b8491b4d933000cfd720893
Rashaad Ramdeen <rashaad.ramdeen@gmail.com>
2014-03-20 03:00:16 -0400

    adding favicon

--- includes/head.php
+++ includes/head.php
@@ -3,6 +3,8 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
 	<!-- Fonts -->
 	<link href='http://fonts.googleapis.com/css?family=Lato:300italic,300,400,700,900' rel='stylesheet' type='text/css'>
 	<!-- Stylesheets -->
