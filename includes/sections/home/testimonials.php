7e85b8cdf141fa25c44f00dd6f0d7869554069fd
zahid <zahid@zahid-G41M-ES2L.(none)>
2014-04-29 01:15:54 -0400

    updated the testimonials for the home page

--- includes/sections/home/testimonials.php
+++ includes/sections/home/testimonials.php
@@ -7,12 +7,13 @@
 						<img src="http://excelblinds.rocketfuse.com/img/about.png" alt="..." class="img-thumbnail">
 					</div> -->
 					<div class="col-sm-12">
-						<h1>Shutters on whole house</h1>
-						<h2></2>
+						<h1>Blinds Installation</h1>
 						<hr>
 						<blockquote>
-							<p>Khalid was wonderful to work with. He took the time to explain every detail and delivered exactly as promised....</p>
-							<footer>Michael J. <cite title="Source Title">(Brampton, ON)</cite></footer>
+							<p>
+								Our experience in dealing with Excel Blinds was exceptional. They helped us choose the right fit for our window...
+							</p>
+							<footer>Neesa R. <cite title="Source Title">(Oakville, ON)</cite></footer>
 						</blockquote>
 					</div>
 				</div>
@@ -25,16 +26,74 @@
 						<img src="http://excelblinds.rocketfuse.com/img/about.png" alt="..." class="img-thumbnail">
 					</div> -->
 					<div class="col-sm-12">
-						<h1>Blinds Installation</h1>
+						<h1>Condo Window Coverings</h1>
 						<hr>
 						<blockquote>
-							<p>Khalid was wonderful to work with. He took the time to explain every detail and delivered exactly as promised....</p>
-							<footer>Jane Doe <cite title="Source Title">(Mississauga, ON)</cite></footer>
+							<p>
+								I contacted the Excel team to get my rental Condo furnished with blinds in time before my tenants moved in...
+							</p>
+							<footer>Emily P.<cite title="Source Title">(Toronto, ON)</cite></footer>
 						</blockquote>
 					</div>
 				</div>
 			</div>
-		</li>	
+		</li>
+		<li>
+			<div class="container">
+				<div class="row">
+<!-- 					<div class="col-sm-3">
+						<img src="http://excelblinds.rocketfuse.com/img/about.png" alt="..." class="img-thumbnail">
+					</div> -->
+					<div class="col-sm-12">
+						<h1>Window Coverings</h1>
+						<hr>
+						<blockquote>
+							<p>
+								I was very pleased with the Excel Team in helping me choose our window coverings. I found the sales consultant...
+							</p>
+							<footer>Tom D.<cite title="Source Title">(Kleinburg, ON)</cite></footer>
+						</blockquote>
+					</div>
+				</div>
+			</div>
+		</li>
+		<li>
+			<div class="container">
+				<div class="row">
+<!-- 					<div class="col-sm-3">
+						<img src="http://excelblinds.rocketfuse.com/img/about.png" alt="..." class="img-thumbnail">
+					</div> -->
+					<div class="col-sm-12">
+						<h1>Honeycomb Blinds</h1>
+						<hr>
+						<blockquote>
+							<p>
+								Our family room was extremely cold during the winter season and due to the cold air, our gas bill was unusually high...
+							</p>
+							<footer>Ryan P.<cite title="Source Title">(Toronto, ON)</cite></footer>
+						</blockquote>
+					</div>
+				</div>
+			</div>
+		</li>
+		<li>
+			<div class="container">
+				<div class="row">
+<!-- 					<div class="col-sm-3">
+						<img src="http://excelblinds.rocketfuse.com/img/about.png" alt="..." class="img-thumbnail">
+					</div> -->
+					<div class="col-sm-12">
+						<h1>Solar Shades</h1>
+						<hr>
+						<blockquote>
+							<p>
+								I am extremely satisfied with the quality of the products and the entire shopping experience. The Solar look exactly...
+							</p>
+							<footer>Sonia N.<cite title="Source Title">(Brampton, ON)</cite></footer>
+						</blockquote>
+					</div>
+				</div>
+			</div>
+		</li>
 	</ul>
-		
 </section>
