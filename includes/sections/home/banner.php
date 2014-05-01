7340f3ffa6510d212e06c757ff446169c81b3c07
Rashaad Ramdeen <rashaad.ramdeen@gmail.com>
2014-05-01 01:20:15 -0400

    updated banner changes

--- includes/sections/home/banner.php
+++ includes/sections/home/banner.php
@@ -15,9 +15,9 @@
 							<div style=" #position: absolute; #top: 50%;display: table-cell; vertical-align: middle;">
 								<div class="inner" class="vcenter" style=" #position: relative; #top: -50%">
 									<p><span class="spacer"></span></p>
-									<h3>SPICE THINGS UP A BIT</h3>
-									<h1>GIVE US A CALL</h1>
-									<p>With years of experience, we know blinds, we know our customers and we know what you will get you excited about your home!</p>
+									<h3>SHED SOME NEW LIGHT</h3>
+									<h1>ON THE SUBJECT</h1>
+									<p>We pride ourselves in delivering the most exquisite, beautiful and high quality blinds that will bring out the beauty in your home.</p>
 									<br>
 									<a href="contact.php"><button class="btn btn-lg">Book an Appointment</button></a>
 								</div>
@@ -29,9 +29,9 @@
 							<div style=" #position: absolute; #top: 50%;display: table-cell; vertical-align: middle;">
 								<div class="inner" class="vcenter" style=" #position: relative; #top: -50%">
 									<p><span class="spacer"></span></p>
-									<h3>SHED SOME NEW LIGHT</h3>
-									<h1>ON THE SUBJECT</h1>
-									<p>We pride ourselves in delivering the most exquisite, beautiful and high quality blinds that will bring out the beauty in your home.</p>
+									<h3>SPICE THINGS UP A BIT</h3>
+									<h1>GIVE US A CALL</h1>
+									<p>With years of experience, we know blinds, we know our customers and we know what you will get you excited about your home!</p>
 									<br>
 									<a href="contact.php"><button class="btn btn-lg">Book an Appointment</button></a>
 								</div>
